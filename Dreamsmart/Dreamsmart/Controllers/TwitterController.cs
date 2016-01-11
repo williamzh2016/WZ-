19ed2b20cb29fb95c81ad30b3816dbd619d77e56
williamzh2016 <netprogramming.william@gmail.com>
2016-01-11 09:59:57 +0800

    Unzipped source folder

---
+++ Dreamsmart/Dreamsmart/Controllers/TwitterController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tweetinvi;
+using Tweetinvi.Core.Interfaces;
+using System.Data;
+using Dreamsmart.Models;
+
+namespace Dreamsmart.Controllers
+{
+    public class TwitterController : Controller
+    {
+
+        private readonly List<Dreamsmart.Models.Tweet> lstTweets = new List<Dreamsmart.Models.Tweet>();
+
+
+        public ActionResult ProcessSubmit(String btnSubmit)
+        {
+            switch (btnSubmit)
+            {
+                case "Retrieve":
+                    if (Request.Form["keyword"] != null)
+                    {
+                        TempData["keyword"] = Request.Form["keyword"];
+                    }
+                    return RedirectToAction("RetrieveTweets");
+                case "Save to DB":
+                    if (Request.Form["keyword"] != null)
+                    {
+                        TempData["keyword"] = Request.Form["keyword"];
+                    }
+                    return RedirectToAction("SaveTweets");
+                default:
+                    return RedirectToAction("ProcessSubmit");
+            }
+
+        }
+
+        // GET: Twitter
+        public ActionResult RetrieveTweets()
+        {
+            string strHashTag = String.Empty;
+            if (TempData["keyword"] == null)
+            {
+                GetTweets();
+            }
+            else
+            {
+                strHashTag = TempData["keyword"].ToString();
+                GetTweets(strHashTag);
+                ViewData["txtHashTag"] = strHashTag;
+            }
+
+            ModelState.Clear();
+            return View(lstTweets);
+        }
+
+        // GET: Twitter
+        public ActionResult LoadSavedTweets()
+        {
+            string strHashTag = String.Empty;
+
+            if (Request.Form["keyword"] != null)
+            {
+                TempData["keyword"] = Request.Form["keyword"];
+            }
+
+            if (TempData["keyword"] == null)
+            {
+                LoadTweets();
+            }
+            else
+            {
+                strHashTag = TempData["keyword"].ToString();
+                LoadTweets(strHashTag);
+                ViewData["txtHashTag"] = strHashTag;
+            }
+
+            ModelState.Clear();
+            return View(lstTweets);
+        }
+
+        public void GetTweets(string keyword = "jakarta")
+        {
+            try
+            {
+                //Set Credentials using the following Parameter : CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+                Auth.SetUserCredentials("UbxfK2d5fQPoJ5ZTuJHdUNGDv", "ZS1CjmCkNDoMsKouDm1WDSxU22yESqs8cqbzRiq2t4hdJYH32d",
+                    "4716392004-vPXcDEeoXbJmfUffVLbYaX4lGtEA8WoXK1zJReA", "1bWqICSAhDGkxT3Zp3C4X9gx1uVG6wjDlc4vvPwz7dnNj");
+                if (keyword.Substring(0, 1) != "#")
+                {
+                    keyword = "#" + keyword;
+                }
+
+                var tokenRateLimits = RateLimit.GetCurrentCredentialsRateLimits();
+                if (tokenRateLimits.ApplicationRateLimitStatusLimit.Remaining > 0)
+                {
+                    var searchParameter = Search.CreateTweetSearchParameter(keyword);
+                    var tweets = Search.SearchTweets(searchParameter);
+
+                    List<ITweet> objList = tweets.ToList();
+
+
+                    foreach (ITweet objTweet in objList)
+                    {
+                        Dreamsmart.Models.Tweet objTw = new Dreamsmart.Models.Tweet();
+                        objTw.Id = objTweet.Id.ToString();
+                        objTw.TweetText = objTweet.Text;
+                        objTw.RetweetCount = objTweet.RetweetCount;
+                        objTw.CreatedBy = objTweet.CreatedBy.ToString();
+                        objTw.CreatedDate = objTweet.CreatedAt;
+                        objTw.HashTag = keyword;
+                        lstTweets.Add(objTw);
+                    }
+                    Session["lstTweets"] = lstTweets;
+                }
+
+            }
+            catch(Exception ex) 
+            {
+                // Exception 
+            }
+
+        }
+
+        public void LoadTweets(string keyword = "")
+        {
+            DataTable objDataTable = new DataTable();
+            Dreamsmart.Models.Tweet objTweet = new Dreamsmart.Models.Tweet();
+            objDataTable = objTweet.Load(keyword);
+            if (objDataTable.Rows.Count > 0)
+            {
+                for (int i = 0; i < objDataTable.Rows.Count; i++)
+                {
+                    Dreamsmart.Models.Tweet objTw = new Dreamsmart.Models.Tweet();
+                    objTw.Id = objDataTable.Rows[i]["Id"].ToString();
+                    objTw.TweetText = objDataTable.Rows[i]["TweetText"].ToString();
+                    objTw.RetweetCount = Convert.ToInt64(objDataTable.Rows[i]["RetweetCount"].ToString());
+                    objTw.CreatedBy = objDataTable.Rows[i]["CreatedBy"].ToString();
+                    objTw.CreatedDate = Convert.ToDateTime(objDataTable.Rows[i]["CreatedDate"].ToString());
+                    objTw.HashTag = objDataTable.Rows[i]["HashTag"].ToString();
+                    lstTweets.Add(objTw);
+                }
+            }
+
+        }
+
+        // Save Tweets
+        public ActionResult SaveTweets()
+        {
+            Save();
+            return RedirectToAction("RetrieveTweets");
+        }
+
+        public void Save()
+        {
+            List<Dreamsmart.Models.Tweet> objLstTweets = new List<Dreamsmart.Models.Tweet>();
+            objLstTweets = (List<Dreamsmart.Models.Tweet>)Session["lstTweets"];
+            foreach (Dreamsmart.Models.Tweet obj in objLstTweets)
+            {
+                obj.Save();
+            }
+            TempData["alertMessage"] = "Tweets have been succesfully saved.";
+        }
+
+    }
+}
---
+++ Dreamsmart/Dreamsmart/Models/Tweet.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Dreamsmart.Models
+{
+    public class Tweet
+    {
+
+        private string strHashTag;
+        private string strId;
+        private string strTweetText;
+        private string strCreatedBy;
+        private DateTime dtCreatedDate;
+        private long lngRetweetCount;
+
+        public String HashTag
+        {
+            get
+            {
+                return strHashTag;
+            }
+            set
+            {
+                strHashTag = value;
+            }
+        }
+
+        public String Id
+        {
+            get
+            {
+                return strId;
+            }
+            set
+            {
+                strId = value;
+            }
+        }
+
+        public string TweetText
+        {
+            get
+            {
+                return strTweetText;
+            }
+            set
+            {
+                strTweetText = value;
+            }
+        }
+
+        public string CreatedBy
+        {
+            get
+            {
+                return strCreatedBy;
+            }
+            set
+            {
+                strCreatedBy = value;
+            }
+        }
+
+        public DateTime CreatedDate
+        {
+            get
+            {
+                return dtCreatedDate;
+            }
+            set
+            {
+                dtCreatedDate = value;
+            }
+        }
+
+        public long RetweetCount
+        {
+            get
+            {
+                return lngRetweetCount;
+            }
+            set
+            {
+                lngRetweetCount = value;
+            }
+        }
+
+        public Boolean Save()
+        {
+            SqlConnection objConn = new SqlConnection();
+            try
+            {
+                objConn.ConnectionString = ConfigurationManager.AppSettings["DBConnection"].ToString();
+                SqlCommand objCommand = new SqlCommand();
+                objCommand.Connection = objConn;
+                objCommand.CommandType = CommandType.StoredProcedure;
+                objCommand.CommandText = "InsertTweet";
+
+                SqlParameter objSqlParameter1 = new SqlParameter("vHashTag", SqlDbType.NVarChar, 100);
+                SqlParameter objSqlParameter2 = new SqlParameter("vId", SqlDbType.NVarChar, 50);
+                SqlParameter objSqlParameter3 = new SqlParameter("vTweetText", SqlDbType.NVarChar, 500);
+                SqlParameter objSqlParameter4 = new SqlParameter("vCreatedBy", SqlDbType.NVarChar, 50);
+                SqlParameter objSqlParameter5 = new SqlParameter("vCreatedDate", SqlDbType.DateTime);
+                SqlParameter objSqlParameter6 = new SqlParameter("vRetweetCount", SqlDbType.Int, 100);
+
+                objSqlParameter1.Direction = ParameterDirection.Input;
+                objSqlParameter2.Direction = ParameterDirection.Input;
+                objSqlParameter3.Direction = ParameterDirection.Input;
+                objSqlParameter4.Direction = ParameterDirection.Input;
+                objSqlParameter5.Direction = ParameterDirection.Input;
+                objSqlParameter6.Direction = ParameterDirection.Input;
+
+                objSqlParameter1.Value = this.HashTag;
+                objSqlParameter2.Value = this.Id;
+                objSqlParameter3.Value = this.TweetText;
+                objSqlParameter4.Value = this.CreatedBy;
+                objSqlParameter5.Value = this.CreatedDate;
+                objSqlParameter6.Value = this.RetweetCount;
+
+                objCommand.Parameters.Add(objSqlParameter1);
+                objCommand.Parameters.Add(objSqlParameter2);
+                objCommand.Parameters.Add(objSqlParameter3);
+                objCommand.Parameters.Add(objSqlParameter4);
+                objCommand.Parameters.Add(objSqlParameter5);
+                objCommand.Parameters.Add(objSqlParameter6);
+
+                objConn.Open();
+                objCommand.ExecuteNonQuery();
+                objConn.Close();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                objConn.Close();
+                return false;
+            }
+        }
+
+        public DataTable Load(String strHashTag = "")
+        {
+            SqlConnection objConn = new SqlConnection();
+            try
+            {
+                objConn.ConnectionString = ConfigurationManager.AppSettings["DBConnection"].ToString();
+                SqlCommand objCommand = new SqlCommand();
+                objCommand.Connection = objConn;
+                objCommand.CommandType = CommandType.StoredProcedure;
+                objCommand.CommandText = "LoadTweet";
+
+                SqlParameter objSqlParameter1 = new SqlParameter("vHashTag", SqlDbType.NVarChar, 100);
+                objSqlParameter1.Direction = ParameterDirection.Input;
+                objSqlParameter1.Value = strHashTag;
+                objCommand.Parameters.Add(objSqlParameter1);
+                objConn.Open();
+
+                DataTable dt = new DataTable();
+                SqlDataAdapter objSqlDataAdapter = new SqlDataAdapter(objCommand);
+                objSqlDataAdapter.Fill(dt);
+
+                objSqlDataAdapter.Dispose();
+                objConn.Close();
+                return dt;
+            }
+            catch (Exception ex)
+            {
+                objConn.Close();
+                return null;
+            }
+        }
+
+    }
+}
